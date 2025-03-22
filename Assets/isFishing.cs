b96160157f3dd1ee78c08d0d3132b0faa1ca45b6
Jessica Nguyen <jnguye27@uoguelph.ca>
2025-03-22 19:41:22 -0400

    Added particles to indicate a fish is on the line.

--- Assets/isFishing.cs
+++ Assets/isFishing.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
+using System.Collections;
 
 public class isFishing : MonoBehaviour {
-    // Declare variable
+    // Declare variables
     private Rigidbody bobblePhysics;
+    private ParticleSystem splash;
 
     void Start() {
-        // Initiate variable
+        // Initiate variables
         bobblePhysics = GetComponent<Rigidbody>();
+        splash = transform.Find("Watersplash").GetComponent<ParticleSystem>();
     }
 
     // Function that checks if the bobble is touching the water
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.name == "Water") {
             // Report this to the terminal
-            Debug.Log("Bobble in Water, Fishing = TRUE");
+            Debug.Log("Bobble in Water, Begin Fishing.");
             
             // Stop the bobble from moving
             Destroy(bobblePhysics);
+
+            // Wait for a fish to appear
+            StartCoroutine(fishAppears(Random.Range(3f, 10f)));
         }
     }
+
+    // Waits a couple seconds before playing a splash particle animation to indicate a fish is on the line
+    IEnumerator fishAppears(float seconds) {
+		yield return new WaitForSeconds(seconds);
+        splash.Play();
+        Debug.Log("Fish on the Line, Begin Capture.");
+	}
 }
